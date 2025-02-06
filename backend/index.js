6328c860dda5fcc26dd177eeead9e0c01cc8c601
saikiranyad <saisaikiran7890@gmail.com>
2025-02-07 01:51:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
     // origin:'http://localhost:5173',
-    origin:['https://hrportal-frontend.onrender.com', 'http://localhost:5179'],
+    origin:['https://hrportal-frontend.onrender.com', 'https://hrportal-admin.onrender.com'],
     credentials:true
 }
 
