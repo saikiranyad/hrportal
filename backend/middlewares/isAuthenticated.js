dfdcbff8dae397adb253e3b43fd2c35525a52886
saikiranyad <saisaikiran7890@gmail.com>
2025-02-07 02:46:02 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -2,7 +2,11 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies?.token; // Ensure cookies exist
+      
+        console.log("Request Headers:", req.headers); // Debugging
+        console.log("Cookies:", req.cookies); // Debugging
+        
+        const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
         if (!token) {
             return res.status(401).json({
                 message: "User not authenticated",
