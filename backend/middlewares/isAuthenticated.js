966a31b9590cad18a8f42d8bfe5d8346b121ab44
saikiranyad <saisaikiran7890@gmail.com>
2025-02-07 11:23:10 +0530

    Update isAuthenticated.js

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -6,7 +6,7 @@ const isAuthenticated = async (req, res, next) => {
         console.log("Request Headers:", req.headers); // Debugging
         console.log("Cookies:", req.cookies); // Debugging
         
-        const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+        const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
         console.log(token,"line10")
         if (!token) {
             return res.status(401).json({
